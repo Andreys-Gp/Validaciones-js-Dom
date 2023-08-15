a87e87c88c906ffd086577c5ad039ab90dbf0ceb
Andreys-Gp <109758319+Andreys-Gp@users.noreply.github.com>
2023-08-15 13:05:02 -0500

    Implement js Dom
    Js validation forms

---
+++ js/apps.js
@@ -0,0 +1,5 @@
+import { valida } from "./validaciones.js";
+const inputs=document.querySelectorAll("input");
+inputs.forEach((input) => {
+       input.addEventListener('blur',(input)=>{valida(input.target)});
+})
